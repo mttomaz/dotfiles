350e8222818987da651df5ef3c948377679ad266
MatheusTT <mttomaz@duck.com>
2025-05-28 16:06:53 -0300

    feat: add ssh keys as templates

--- install.sh
+++ install.sh
@@ -4,11 +4,17 @@
 # are for my personal use, feel free to comment things you dont want to install.
 # I have a AMD GPU, so if you use Nvidia you have to
 # configure and install more things (specially on wayland).
-# Initializing and applying dotfiles
 
 if command -v pacman >/dev/null; then
-  sudo pacman -Syyu chezmoi
+  sudo pacman -Syyu --needed chezmoi bitwarden-cli
 fi
+
+if ! bw login --check >/dev/null; then
+  BW_SESSION="$(bw login --raw)"
+else
+  BW_SESSION="$(bw unlock --raw)"
+fi
+export BW_SESSION
 
 echo -e "\n\033[1;32mInitialing chezmoi ...\033[0m"
 chezmoi init --apply mttomaz
