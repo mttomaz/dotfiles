1dfe316f3c414a04913826be2ae546d385710559
mttomaz <mttomaz@duck.com>
2025-05-29 15:05:28 -0300

    chore: update my name

--- home/dot_config/nvim/lua/plugins/cord.lua
+++ home/dot_config/nvim/lua/plugins/cord.lua
@@ -1,16 +1,16 @@
 return {
-	"vyfor/cord.nvim",
-	build = ":Cord update",
-	opts = {
-		buttons = {
-			{
-				label = "dotfiles",
-				url = "https://github.com/MatheusTT/dotfiles",
-			},
-			{
-				label = "wallpapers",
-				url = "https://github.com/MatheusTT/wallpapers",
-			},
-		},
-	},
+  "vyfor/cord.nvim",
+  build = ":Cord update",
+  opts = {
+    buttons = {
+      {
+        label = "dotfiles",
+        url = "https://github.com/mttomaz/dotfiles",
+      },
+      {
+        label = "wallpapers",
+        url = "https://github.com/mttomaz/wallpapers",
+      },
+    },
+  },
 }
