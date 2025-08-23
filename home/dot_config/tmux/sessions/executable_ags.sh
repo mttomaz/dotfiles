3e457bc6fd43c92aae5dae94c533d8f5b4413edd
mttomaz <mttomaz@duck.com>
2025-08-22 23:42:18 -0300

    chore(ags): update dependencies

--- home/dot_config/tmux/sessions/executable_ags.sh
+++ home/dot_config/tmux/sessions/executable_ags.sh
@@ -5,6 +5,6 @@ SESSION_PATH="$HOME/.config/ags"
 tmux new-session -ds "$1" -c "$SESSION_PATH" -n "edit"
 
 tmux new-window -t "$1":2 -n "gjs" -c "$SESSION_PATH"
-pgrep -x gjs || tmux send-keys -t "$1":2 "./launch.sh" C-m
+pgrep -x gjs || tmux send-keys -t "$1":2 "sh ./launch.sh" C-m
 tmux new-window -t "$1":3 -n "other" -c "$SESSION_PATH"
 tmux select-window -t "$1":1
